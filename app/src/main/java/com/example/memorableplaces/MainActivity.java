5f036a2681ed3dd027a969281509ff9b3c76596e
Ricky Hu <ricky9667@gmail.com>
2020-03-30 16:50:02 +0800

    Add permanent save

--- app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,10 +26,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.memorableplaces", Context.MODE_PRIVATE);
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
         ListView listView = findViewById(R.id.listView);
 
-        places.add("Add a new place...");
-        locations.add(new LatLng(0, 0));
+        places.clear();
+        latitudes.clear();
+        longitudes.clear();
+        locations.clear();
+
+        try {
+            places = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("places", ObjectSerializer.serialize(new ArrayList<>())));
+            latitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lats", ObjectSerializer.serialize(new ArrayList<>())));
+            longitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lons", ObjectSerializer.serialize(new ArrayList<>())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (places.size() > 0 && latitudes.size() > 0 && longitudes.size() > 0) {
+            if (places.size() == latitudes.size() && places.size() == longitudes.size()) {
+                for (int i=0; i<places.size(); i++) {
+                    locations.add(new LatLng(Double.parseDouble(latitudes.get(i)), Double.parseDouble(longitudes.get(i))));
+                }
+            }
+        } else {
+            places.add("Add a new place...");
+            locations.add(new LatLng(0, 0));
+        }
 
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, places);
         listView.setAdapter(adapter);
